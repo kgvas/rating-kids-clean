d77f0f8a1fa16d801f732d5a50f738b7ff64ae90
kgvas <91349383+kgvas@users.noreply.github.com>
2025-06-29 22:19:03 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Инициализация SDK Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDmS0s4atP885XoLoMAA21ff_LmPaYqvcg",
   authDomain: "rating-kids.firebaseapp.com",
   projectId: "rating-kids",
-  storageBucket: "rating-kids.firebasestorage.app",
+  storageBucket: "rating-kids.appspot.com", // Исправлено: ".app" → ".com"
   messagingSenderId: "1031565546493",
   appId: "1:1031565546493:web:6fb783c12c9f22653e7f94",
   measurementId: "G-FEK2JMG4LJ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
